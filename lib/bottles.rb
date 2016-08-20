8b1312e65b03088c795ba3d2a81796945b485500
Camille Appert <camille@appert.me>
2016-08-20 20:09:29 +0200

    pass third test, you're now thinking with plurals
    and the award to worst code goes to...

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,7 +1,7 @@
 class Bottles
   def verse(number)
     return "#{number} bottles of beer on the wall, #{number} bottles of beer.
-Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+Take one down and pass it around, #{number - 1} bottle#{2 == number ? '' : 's'} of beer on the wall.
 "
   end
 end
