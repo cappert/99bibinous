3975f574deac92d805f756bbc0f57c4a760a0fd1
Camille Appert <camille@appert.me>
2016-08-20 20:23:50 +0200

    refactor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,7 +1,18 @@
 class Bottles
   def verse(number)
-    return "#{number} bottles of beer on the wall, #{number} bottles of beer.
-Take one down and pass it around, #{number - 1} bottle#{2 == number ? '' : 's'} of beer on the wall.
-"
+    return first_stanza(number) + "\n" + second_stanza(number)
   end
+  
+  def first_stanza(number)
+    "#{number} bottles of beer on the wall, #{number} bottles of beer."
+  end
+
+  def second_stanza(number)
+    "Take one down and pass it around, #{number - 1} bottle#{2 == number ? '' : 's'} of beer on the wall.\n"
+  end
+  
+end
+
+if __FILE__ == $0
+  puts Bottles.new.verse(99)
 end
