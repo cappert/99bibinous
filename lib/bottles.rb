1685f0d169a3fa263ef46012585bd67e9305865c
Camille Appert <camille@appert.me>
2016-08-20 20:04:05 +0200

    finish second test test_another_verse
    actual code lolz

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,7 +1,7 @@
 class Bottles
   def verse(number)
-    return "99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
+    return "#{number} bottles of beer on the wall, #{number} bottles of beer.
+Take one down and pass it around, #{number - 1} bottles of beer on the wall.
 "
   end
 end
