95968a277e4f30fd4b32a1a20f3194bb5253103a
Camille Appert <camille@appert.me>
2016-08-22 17:32:52 +0200

    this is terribad, don't look
    === start of day 2 ===

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,18 +1,38 @@
 class Bottles
   def verse(number)
-    return first_stanza(number) + "\n" + second_stanza(number)
+    return first_stanza(number) + "\n" + second_stanza(number) + "\n"
   end
   
   def first_stanza(number)
-    "#{number} bottles of beer on the wall, #{number} bottles of beer."
+    "#{number} bottle#{is_bottle_plural?(number)} of beer on the wall, #{number} bottle#{is_bottle_plural?(number)} of beer."
   end
 
   def second_stanza(number)
-    "Take one down and pass it around, #{number - 1} bottle#{is_bottle_plural?(number)} of beer on the wall.\n"
+    "Take #{1 == number ? "it" : "one" } down and pass it around, #{several_one_or_zero_bottles(number - 1)} of beer on the wall."
   end
   
+  def several_one_or_zero_bottles(number_minus_one)
+    return number_or_no(number_minus_one) + " bottle" + is_bottle_plural?(number_minus_one)
+  end
+  
+  def number_or_no(number_minus_one)
+    if 0 == number_minus_one
+      return 'no more' 
+    else
+      return number_minus_one.to_s
+    end
+  end
+    
+  
   def is_bottle_plural?(number)
-    2 == number ? '' : 's'
+    if 1 == number
+      return ''
+    end
+    if 0 == number
+      return 's'
+    else
+      return 's'
+    end
   end
   
 end
